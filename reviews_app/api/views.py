6996392a5eca1103dcc5ba9e403d68ec4896c63c
Pinguinrakete <Pinguinrakete@gmail.com>
2025-09-01 01:00:23 +0200

    add patch to ReviewSingleView in reviews_app

--- reviews_app/api/views.py
+++ reviews_app/api/views.py
@@ -4,7 +4,7 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from reviews_app.models import Review
-from .serializers import ReviewSerializer
+from .serializers import ReviewSerializer, ReviewSinglePatchSerializer
 
 class ReviewsView(APIView):
     permission_classes = [IsAuthenticated]
@@ -26,6 +26,21 @@ class ReviewsView(APIView):
 
 class ReviewSingleView(APIView):
     permission_classes = [IsAuthenticated]
+
+    def patch(self, request, id):
+        try:
+            review = Review.objects.get(pk=id)
+        except Review.DoesNotExist:
+            return Response({"detail": "Review not found."}, status=status.HTTP_404_NOT_FOUND)
+
+        if request.user.id != review.reviewer_id:
+            return Response({"detail": "Only the reviewer can update this review."}, status=status.HTTP_403_FORBIDDEN)
+        
+        serializer = ReviewSinglePatchSerializer(review, data=request.data, partial=True, context={'request': request})
+
+        if serializer.is_valid():
+            review = serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
 
     def delete(self, request, id):
         try:
