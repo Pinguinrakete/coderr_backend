1df2a97e4ca809348822541224ab347065e32b69
Pinguinrakete <Pinguinrakete@gmail.com>
2025-08-31 14:00:34 +0200

    add model rewiew to reviews_app

--- reviews_app/models.py
+++ reviews_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from auth_app.models import Account
 
-# Create your models here.
+class Rewiew(models.Model):
+    business_user = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='received-reviews')
+    reviewer = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='given-reviews')
+    rating = models.PositiveIntegerField(default=0)
+    description = models.TextField(blank=True, default="")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        constraints = [models.UniqueConstraint(fields=['business_user', 'reviewer'], name='unique-review-per-business_user')]
