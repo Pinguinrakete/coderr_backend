c53fe06c76f774cd2db82f924f8b326f275683f2
Pinguinrakete <Pinguinrakete@gmail.com>
2025-09-23 19:15:02 +0200

    remove migrations of gitignore

--- auth_app/api/views.py
+++ auth_app/api/views.py
@@ -5,7 +5,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 from .serializers import RegistrationSerializer, LoginSerializer
-from django.db import IntegrityError
+
 """ 
 This handles the user registration. 
 
@@ -20,21 +20,16 @@ class RegistrationView(APIView):
         serializer = RegistrationSerializer(data=request.data)
 
         if serializer.is_valid():
-            try:
-                saved_account = serializer.save()
-                token, created = Token.objects.get_or_create(user=saved_account)
-                data = {
-                    'token': token.key,
-                    'username': saved_account.username,
-                    'email': saved_account.email,
-                    'user_id': saved_account.id
-                }
-                return Response(data, status=status.HTTP_201_CREATED)
-        
-            except Exception as e:
-                # Debug-Ausgabe
-                return Response({'error': str(e)}, status=500)
-       
+            saved_account = serializer.save()
+            token, created = Token.objects.get_or_create(user=saved_account)
+            data = {
+                'token': token.key,
+                'username': saved_account.username,
+                'email': saved_account.email,
+                'user_id': saved_account.id
+            }
+            return Response(data, status=status.HTTP_201_CREATED)
+              
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 """
